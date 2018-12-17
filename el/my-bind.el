f03e2b7d836cd4add57423c5836d3b0feb595de7
zimoun <zimoun.toutoune@gmail.com>
2018-12-17 10:28:33 +0100

    reorder hippie-expand

--- el/my-bind.el
+++ el/my-bind.el
@@ -29,6 +29,19 @@
 
 ;; Use `hippie-expand' instead of `dabbrev-expand'
 (global-set-key "\M-/" 'hippie-expand)
+(setq hippie-expand-try-functions-list
+      '(
+        try-expand-dabbrev
+        try-expand-all-abbrevs
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-list
+        try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol
+        ))
 
 ;;;;
 ;;;; Already binded by M-;
