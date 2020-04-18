b0a028675aa63d7ec733f400314c87e552d9c6b3
zimoun <zimon.toutoune@gmail.com>
2020-04-18 23:06:22 +0200

    fix my-stow.sh

--- my-stow.sh
+++ my-stow.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-mydots=/home/simon/work/my-conf
+# Avoid issues
+mkdir -p ~/.config/guix
+mkdir -p ~/.emacs.d/
+mkdir -p ~/.local/bin/
+
+if [ -f ~/.bashrc ]
+then
+    mv ~/.bashrc ~/.bashrc.bak
+fi
 
 guix environment --ad-hoc stow \
-     -- stow -d $mydots -t /home/simon . -R 
+     -- stow -t $HOME . -R -v
